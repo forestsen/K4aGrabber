ff1c7fec761b2a1780a948362202e34e0e336c76
无雨森 <forestsen@vip.qq.com>
2019-10-10 17:42:21 +0800

    Add the calibration refine process to the PCL_Grabber_TwoKinects project.

--- PCL_Grabber_TwoKinects/Source_pcl.cpp
+++ PCL_Grabber_TwoKinects/Source_pcl.cpp
@@ -13,11 +13,18 @@
 #include <pcl/point_types.h>
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/common/transforms.h>
+#include <pcl/io/ply_io.h>
 #include <pcl/filters/passthrough.h>
+#include <pcl/filters/bilateral.h>
+#include <pcl/filters/fast_bilateral.h>
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+
+#include <Open3D/Geometry/PointCloud.h>
+#include <Open3d/Registration/ColoredICP.h>
+#include <Open3D/Open3D.h>
 
 #include "k4a_grabber.h"
 
@@ -50,26 +57,68 @@ Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic> load_csv(const std::stri
 	return Eigen::Map<const Eigen::Matrix<typename Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>::Scalar, Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>::RowsAtCompileTime, Eigen::Matrix<DataType, Eigen::Dynamic, Eigen::Dynamic>::ColsAtCompileTime, Eigen::RowMajor>>(values.data(), rows, values.size() / rows);
 }
 
+void convertToOpenCDPointCloud(pcl::PointCloud<PointType>::ConstPtr cloud, open3d::geometry::PointCloud &pointCloud)
+{
+	double x, y, z, r, g, b;
+	pointCloud.Clear();
+	for (const auto &point : cloud->points)
+	{
+		x = point.x;
+		y = point.y;
+		z = point.z;
+		r = point.r / 256.0;
+		g = point.g / 256.0;
+		b = point.b / 256.0;
+
+		pointCloud.points_.push_back(Eigen::Vector3d(x, y, z));
+		pointCloud.colors_.push_back(Eigen::Vector3d(r, g, b));
+	}
+}
+
+Eigen::Matrix4d colorCloudRegistrationOptimization(pcl::PointCloud<PointType>::ConstPtr pcl_source, pcl::PointCloud<PointType>::ConstPtr pcl_target, const Eigen::Matrix4d &init_transformation)
+{
+	open3d::geometry::PointCloud open3d_source_original, open3d_target_original;
+	open3d::geometry::PointCloud open3d_source_current, open3d_target_current;
+
+	convertToOpenCDPointCloud(pcl_source, open3d_source_original);
+	convertToOpenCDPointCloud(pcl_target, open3d_target_original);
+
+	std::array<double, 3> voxel_radius = { 0.04, 0.02, 0.01};
+	std::array<int, 3> max_iter = { 50, 30, 14};
+	open3d::registration::RegistrationResult result;
+	Eigen::Matrix4d current_transformation = init_transformation;
+
+	open3d_source_current = open3d_source_original;
+	open3d_target_current = open3d_target_original;
+
+	open3d::geometry::PointCloud open3d_source_down;
+	open3d::geometry::PointCloud open3d_target_down;
+
+	double radius = 0.01;
+
+	open3d_source_down = *open3d_source_current.VoxelDownSample(radius);
+	open3d_target_down = *open3d_target_current.VoxelDownSample(radius);
+
+	open3d_source_down.EstimateNormals(open3d::geometry::KDTreeSearchParamHybrid(radius * 2, 30));
+	open3d_target_down.EstimateNormals(open3d::geometry::KDTreeSearchParamHybrid(radius * 2, 30));
+
+	result = open3d::registration::RegistrationColoredICP(open3d_source_down, open3d_target_down,
+		radius, current_transformation,
+		open3d::registration::ICPConvergenceCriteria(1e-16, 1e-16, 500));
+
+	return result.transformation_;
+}
+
 int main(int argc, char **argv)
 {
 	if (argc < 7)
 	{
-		throw std::runtime_error("Required arguments: \nframe_sub_master.csv frame_sub_marker.csv master_znear master_zfar sub_znear sub_zfar");
+		throw std::runtime_error("Required arguments: \nframe_sub_master.csv");
 	}
 	string frame_sub_master_file = argv[1];
 	cout << "Reading frame_sub_master.csv" << endl;
-	Transform<float, 3, Affine> transformation_sub_master;
-	transformation_sub_master.matrix() = load_csv<float>(frame_sub_master_file);
-
-	string frame_sub_marker_file = argv[2];
-	cout << "Reading frame_sub_marker.csv" << endl;
-	Transform<float, 3, Affine> transformation_sub_marker;
-	transformation_sub_marker.matrix() = load_csv<float>(frame_sub_marker_file);
-
-	float master_znear = std::stof(argv[3]);
-	float master_zfar = std::stof(argv[4]);
-	float sub_znear = std::stof(argv[5]);
-	float sub_zfar = std::stof(argv[6]);
+	Transform<double, 3, Affine> transformation_sub_master;
+	transformation_sub_master.matrix() = load_csv<double>(frame_sub_master_file);
 
 	const uint32_t deviceCount = k4a::device::get_installed_count();
 	if (deviceCount == 0)
@@ -87,34 +136,26 @@ int main(int argc, char **argv)
 	// Retrieved Point Cloud Callback Function
 	boost::mutex mutex_master;
 	boost::function<void(const pcl::PointCloud<PointType>::ConstPtr&)> function_master =
-		[&cloud_master, &mutex_master, &master_znear, &master_zfar](const pcl::PointCloud<PointType>::ConstPtr& ptr)
+		[&cloud_master, &mutex_master](const pcl::PointCloud<PointType>::ConstPtr& ptr)
 	{
 		boost::mutex::scoped_lock lock(mutex_master);
 
 		/* Point Cloud Processing */
 
 		cloud_master = ptr->makeShared();
-		PassThrough<PointType> pt;
-		pt.setInputCloud(cloud_master);
-		pt.setFilterFieldName("z");
-		pt.setFilterLimits(master_znear, master_zfar);
-		pt.filter(*cloud_master);
+
 	};
 
 	boost::mutex mutex_sub;
 	boost::function<void(const pcl::PointCloud<PointType>::ConstPtr&)> function_sub =
-		[&cloud_sub, &mutex_sub, &sub_znear, &sub_zfar](const pcl::PointCloud<PointType>::ConstPtr& ptr)
+		[&cloud_sub, &mutex_sub](const pcl::PointCloud<PointType>::ConstPtr& ptr)
 	{
 		boost::mutex::scoped_lock lock(mutex_sub);
 
 		/* Point Cloud Processing */
 
 		cloud_sub = ptr->makeShared();
-		PassThrough<PointType> pt;
-		pt.setInputCloud(cloud_sub);
-		pt.setFilterFieldName("z");
-		pt.setFilterLimits(sub_znear, sub_zfar);
-		pt.filter(*cloud_sub);
+
 	};
 
 	// KinectAzureDKGrabber
@@ -144,10 +185,6 @@ int main(int argc, char **argv)
 	viewer->setCameraParameters(intrinsics_eigen, extrinsics_eigen);
 
 	pcl::PointCloud<PointType>::Ptr cloud(new PointCloud<PointType>);
-	pcl::PointCloud<PointType>::Ptr cloud_transformed(new PointCloud<PointType>);
-	pcl::PointCloud<PointType>::Ptr cloud_axis_master(new PointCloud<PointType>);
-	pcl::PointCloud<PointType>::Ptr cloud_axis_sub(new PointCloud<PointType>);
-	pcl::PointCloud<PointType>::Ptr cloud_axis_marker(new PointCloud<PointType>);
 
 	float aspect = 0.475f;
 	
@@ -155,10 +192,12 @@ int main(int argc, char **argv)
 	transformation_sub_master.matrix()(1, 3) *= aspect;
 	transformation_sub_master.matrix()(2, 3) *= aspect;
 
-	
-	transformation_sub_marker.matrix()(0, 3) *= aspect;
-	transformation_sub_marker.matrix()(1, 3) *= aspect;
-	transformation_sub_marker.matrix()(2, 3) *= aspect;
+	bool refineResult = false;
+
+	Eigen::Matrix4d init_transformation;
+	init_transformation = transformation_sub_master.matrix();
+
+	Eigen::Matrix4d refinedTransformation;
 
 	while (!viewer->wasStopped())
 	{
@@ -170,9 +209,17 @@ int main(int argc, char **argv)
 		if (lock_master.owns_lock() && cloud_master &&
 			lock_sub.owns_lock() && cloud_sub)
 		{
-			transformPointCloud<PointType, float>(*cloud_master, *cloud_transformed, transformation_sub_master, true);
+			if (!refineResult)
+			{
+				std::cout << "Start Calibration Optimization." << std::endl;
+				refinedTransformation = colorCloudRegistrationOptimization(cloud_master, cloud_sub, init_transformation);
+				refineResult = true;
+				std::cout << "Calibration optimization process over." << std::endl;
+			}
 
-			*cloud = *cloud_sub + *cloud_transformed;
+			transformPointCloud(*cloud_master, *cloud_master, refinedTransformation, true);
+
+			*cloud = *cloud_sub + *cloud_master;
 
 			// Update Point Cloud
 			if (!viewer->updatePointCloud(cloud, "cloud"))
@@ -181,7 +228,6 @@ int main(int argc, char **argv)
 			}
 		}
 	}
-
 	// Stop Grabber
 	grabber_sub->stop();
 	grabber_master->stop();
@@ -192,5 +238,6 @@ int main(int argc, char **argv)
 		connection_sub.disconnect();
 		connection_master.disconnect();
 	}
+
 	return 0;
 }
